b92c668e899f6cb9ac7399b248262d9fb3781a4a
genie9 <jenny.lyjina@gmail.com>
2017-05-09 22:40:10 +0300

    separate file for toggle-popover

--- static/js/app/controllers/search_controller.js
+++ static/js/app/controllers/search_controller.js
@@ -349,17 +349,3 @@ SearchApp.controller("SearchController", ["$scope", "$rootScope", "$sce", "$loca
         $scope.search();
     }
 }]);
-SearchApp.directive('toggle', function(){
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs){
-            if (attrs.toggle=="popover"){
-                $(element).popover({
-                    container:'body',
-                    html:true,
-                    placement:'bottom'
-                });
-            }
-        }
-    };
-})
